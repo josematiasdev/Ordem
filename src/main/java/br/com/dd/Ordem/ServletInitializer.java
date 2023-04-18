71af06c30d523e96d21e47e1a7d06284e8e943db
Matias <jose.matias@academico.uncisal.edu.br>
2023-04-17 23:35:05 -0300

    adicionando itens base do projeto

---
+++ src/main/java/br/com/dd/Ordem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.dd.Ordem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OrdemApplication.class);
+	}
+
+}
---
+++ src/test/java/br/com/dd/Ordem/OrdemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dd.Ordem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
